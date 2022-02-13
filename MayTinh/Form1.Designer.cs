2f1c423e429f7d34a8b4aff641d2aa3125af8124
noonoo169 <88936808+noonoo169@users.noreply.github.com>
2022-02-13 12:18:46 +0700

    new

---
+++ MayTinh/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace MayTinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.Expression = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn0
+            // 
+            resources.ApplyResources(this.btn0, "btn0");
+            this.btn0.Name = "btn0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn6
+            // 
+            resources.ApplyResources(this.btn6, "btn6");
+            this.btn6.Name = "btn6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn5
+            // 
+            resources.ApplyResources(this.btn5, "btn5");
+            this.btn5.Name = "btn5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn4
+            // 
+            resources.ApplyResources(this.btn4, "btn4");
+            this.btn4.Name = "btn4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn3
+            // 
+            resources.ApplyResources(this.btn3, "btn3");
+            this.btn3.Name = "btn3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn2
+            // 
+            resources.ApplyResources(this.btn2, "btn2");
+            this.btn2.Name = "btn2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn1
+            // 
+            resources.ApplyResources(this.btn1, "btn1");
+            this.btn1.Name = "btn1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn9
+            // 
+            resources.ApplyResources(this.btn9, "btn9");
+            this.btn9.Name = "btn9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button11
+            // 
+            resources.ApplyResources(this.button11, "button11");
+            this.button11.Name = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn7
+            // 
+            resources.ApplyResources(this.btn7, "btn7");
+            this.btn7.Name = "btn7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // btnClear
+            // 
+            resources.ApplyResources(this.btnClear, "btnClear");
+            this.btnClear.Name = "btnClear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnEqual
+            // 
+            resources.ApplyResources(this.btnEqual, "btnEqual");
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // result
+            // 
+            resources.ApplyResources(this.result, "result");
+            this.result.Name = "result";
+            // 
+            // Expression
+            // 
+            resources.ApplyResources(this.Expression, "Expression");
+            this.Expression.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Expression.Name = "Expression";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Expression);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn0);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label Expression;
+    }
+}
+
---
+++ MayTinh/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MayTinh
+{
+    
+    public partial class Form1 : Form
+    {
+        double values = 0;
+        string op ;
+        bool flag = false; // xoá số cũ sau khi bấm phép tính 
+        public Form1()
+        {
+            InitializeComponent();
+        }   
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if( ( result.Text == "0" ) || ( flag == true ) )
+            {
+                result.Text = "";
+            }
+            Button b = (Button)sender;
+            if( b.Text == ".")
+            {
+                if(!result.Text.Contains("."))
+                {
+                    result.Text = result.Text + b.Text;
+                }
+            }
+            else
+            {
+                result.Text = result.Text + b.Text;
+            }
+            flag = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Expression.Text = "";
+            result.Text = "0";
+            values = 0;
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if ( values != 0)
+            {
+                btnEqual.PerformClick();
+            }
+            else
+            {
+                values = Convert.ToDouble(result.Text);
+            }
+            op = b.Text;
+            Expression.Text = (values).ToString() + op ;
+            flag = true;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            Expression.Text += result.Text ;
+
+            if ( op == "+" )
+            {
+                result.Text = (values + Convert.ToDouble(result.Text)).ToString();
+            }
+            if ( op == "-" )
+            {
+                result.Text = (values - Convert.ToDouble(result.Text)).ToString();
+            }
+            if ( op == "*" )
+            {
+                result.Text = (values * Convert.ToDouble(result.Text)).ToString();
+            }           
+            if ( op == "/" )
+            {
+                if ( result.Text != "0" )
+                {
+                    result.Text = (values / Convert.ToDouble(result.Text)).ToString();
+                }
+                else
+                {
+                    result.Text = "Cannot divide by zero";
+                    btnClear.PerformClick();
+                }
+            }
+            values = Convert.ToDouble(result.Text);
+            op = "";
+        }
+
+    }
+}
